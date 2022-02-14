606b2c0be32b5449995df014a2cf60c405c79df9
Jason Kundi <94011909+JasonKundi@users.noreply.github.com>
2022-02-14 14:37:31 +0000

    Updated push

--- index.js
+++ index.js
@@ -3,129 +3,152 @@ const state = {
     {
       id: "001-beetroot",
       name: "beetroot",
-      price: 0.35
+      price: 0.35,
     },
     {
       id: "002-carrot",
       name: "carrot",
-      price: 0.35
+      price: 0.35,
     },
     {
       id: "003-apple",
       name: "apple",
-      price: 0.35
+      price: 0.35,
     },
     {
       id: "004-apricot",
       name: "apricot",
-      price: 0.35
+      price: 0.35,
     },
     {
       id: "005-avocado",
       name: "avocado",
-      price: 0.35
+      price: 0.35,
     },
     {
       id: "006-bananas",
       name: "bananas",
-      price: 0.35
+      price: 0.35,
     },
     {
       id: "007-bell-pepper",
       name: "bell pepper",
-      price: 0.35
+      price: 0.35,
     },
     {
       id: "008-berry",
       name: "berry",
-      price: 0.35
+      price: 0.35,
     },
     {
       id: "009-blueberry",
       name: "blueberry",
-      price: 0.35
+      price: 0.35,
     },
     {
       id: "010-eggplant",
       name: "eggplant",
-      price: 0.35
-    }
+      price: 0.35,
+    },
   ],
-  cart: []
+  cart: [],
 };
 
+const storeItem = document.querySelector("#storeItem");
 
 
+function render() {
+  clear();
+  renderStoreItem();
+  renderCartItem();
+}
+
+function clear() {
+  storeItem.innerHTML = "";
+  cartItem.innerHTML = "";
+}
 
 function renderStoreItem() {
-  const storeItem = document.querySelector("ul")
-  storeItem.setAttribute("class", "item-list store--item-list" )
-
   for (const item of state.items) {
-    const li = document.createElement("li")
-    storeItem.appendChild(li)
+    const li = document.createElement("li");
+    storeItem.appendChild(li);
 
-    const div = document.createElement("div")
-    div.setAttribute("class", "store--item--icon")
-    li.appendChild(div)
+    const div = document.createElement("div");
+    div.setAttribute("class", "store--item--icon");
+    li.appendChild(div);
 
-    const img = document.createElement("img")
-    img.src = `assets/icons/${item.id}.svg`
-    img.alt = `${item.name}`
-    div.appendChild(img)
+    const img = document.createElement("img");
+    img.src = `assets/icons/${item.id}.svg`;
+    img.alt = `${item.name}`;
+    div.appendChild(img);
 
-    const button = document.createElement("button")
-    li.appendChild(button)
-    button.innerText = "ADD TO CART"
-    
+    const button = document.createElement("button");
+    li.appendChild(button);
+    button.innerText = "ADD TO CART";
+    button.addEventListener("click", function () {
+      const existingOrderItem = state.items.find((i) => i.item === item);
+      if (existingOrderItem !== undefined) {
+        existingOrderItem.quantity++;
+      } else {
+        state.cart.push({
+          quantity: 1,
+          item: item,
+        });
+      }
+      render();
+    });
   }
-  button.addEventListener('click', function(item) {
-    cart.push(item)
-  })
 }
 
+const cartItem = document.querySelector("#cartList");
+    
 
 function renderCartItem() {
+  for (const orderedItem of state.cart) {
 
-  const cartItemContainer = document.createElement("div")
-  cartItemContainer.setAttribute("class", "cart--item-list-container")
+    console.log("Oioiiii" , orderedItem)
 
-  const cartItem = document.createElement("ul")
-  cartItem.setAttribute("class", "item-list cart--item-list")
-  cartItemContainer.appendChild(cartItem)
+    const li = document.createElement("li");
+    li.innerText = orderedItem.quantity + "x" + orderedItem.item.name
+    cartItem.append(li);
 
-  const li = document.createElement("li")
-  cartItem.appendChild("li")
+    const img = document.createElement("img");
+    img.setAttribute("class", "cart--item-icon");
+    img.src = `assets/icons/${orderedItem.item.id}.svg`;
+    img.alt = `${orderedItem.item.name}`;
+    li.append(img);
 
-  const img = document.createElement("img")
-    img.src = `assets/icons/${item.id}.svg`
-    img.alt = `${item.name}`
-    li.appendChild(img)
+    const p = document.createElement("p");
+    p.innerText = `${orderedItem.item.name}`;
+    li.append(p);
 
-    const p = document.createElement("p")
-    p.innerText = `${item.name}`
-    img.appendChild(p)
+    const removeButton = document.createElement("button");
+    removeButton.setAttribute("class", "quantity-btn remove-btn center");
+    removeButton.innerText = "-";
+    removeButton.addEventListener("click", function(){
+      orderedItem.quantity--
+      if (orderedItem.quantity ===0) {
+        const orderedItemIndex = state.cart.findIndex( i => i===orderedItem)
+        state.cart.splice(orderedItemIndex, 1)
+      }
+      render()
+    })
+    li.append(removeButton);
 
-    const removeButton = document.createElement("button")
-    removeButton.setAttribute("class", "quantity-btn remove-btn center")
-    removeButton.innerText = "-"
-    p.appendChild(removeButton)
+    const textCenter = document.createElement("span");
+    textCenter.setAttribute("class", "quantity-text center");
+    textCenter.innerText = orderedItem.quantity
+    li.append(textCenter);
 
-    const textCenter = document.createElement("span")
-    textCenter.setAttribute("class", "quantity-text center")
-    removeButton.appendChild(textCenter)
-
-    const addButton = document.createElement("button")
-    addButton.setAttribute("class", "quantity-btn add-btn center")
-    addButton.innerText = "+"
-    textCenter.appendChild(addButton)
-
-    cart.append(cartItemContainer)
-
-
-
+    const addButton = document.createElement("button");
+    addButton.setAttribute("class", "quantity-btn add-btn center");
+    addButton.innerText = "+";
+    addButton.addEventListener("click", function() {
+      orderedItem.quantity++
+      render()
+    })
+    li.append(addButton);
+  }
 }
 
-
-renderStoreItem()
-shoppingCart()
+render();
